da34b71ea72020098399d4d4e78730315abf7496
camercu <unterbergerc@live.com>
2015-11-10 20:29:36 +0300

    Add README; make 'push_dotfiles.sh' path-independent
    added `pwd` to `ls` command so it won't need to be in .dotfiles to run

---
+++ README.md
@@ -0,0 +1,9 @@
+#Personal Settings & Dotfiles
+
+This repository holds all of my dotfiles and custom user settings for bash, git,
+Sublime Text 3, and gnupg.
+
+## Usage
+To link these dotfiles to their proper locations, run `./push_dotfiles.sh`.
+  *NOTE:* You should back up your old dotfiles before running this script, as it
+  will delete them.
--- push_dotfiles.sh
+++ push_dotfiles.sh
@@ -10,11 +10,11 @@ rm -f ~/.gnupg/gpg.conf
 rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
 
-ln -s ~/.dotfiles/bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/git-prompt.sh ~/.git-prompt.sh
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/.dotfiles/inputrc ~/.inputrc
-ln -s ~/.dotfiles/gpg.conf ~/.gnupg/
-ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -s "$(pwd)/bash_profile" ~/.bash_profile
+ln -s "$(pwd)/bashrc" ~/.bashrc
+ln -s "$(pwd)/git-prompt.sh" ~/.git-prompt.sh
+ln -s "$(pwd)/gitconfig" ~/.gitconfig
+ln -s "$(pwd)/gitignore_global" ~/.gitignore_global
+ln -s "$(pwd)/inputrc" ~/.inputrc
+ln -s "$(pwd)/gpg.conf" ~/.gnupg/
+ln -s "$(pwd)/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
