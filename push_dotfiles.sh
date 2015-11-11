e9b29591c9b2453e2248d760bcb5a71c42cbdc8e
camercu <unterbergerc@live.com>
2015-11-11 07:32:40 +0300

    fixed path-independence of 'push_dotfiles.sh'

--- push_dotfiles.sh
+++ push_dotfiles.sh
@@ -9,12 +9,13 @@ rm -f ~/.inputrc
 rm -f ~/.gnupg/gpg.conf
 rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
+DOTFILE_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd ) # absolute path to dir
 
-ln -s "$(pwd)/bash_profile" ~/.bash_profile
-ln -s "$(pwd)/bashrc" ~/.bashrc
-ln -s "$(pwd)/git-prompt.sh" ~/.git-prompt.sh
-ln -s "$(pwd)/gitconfig" ~/.gitconfig
-ln -s "$(pwd)/gitignore_global" ~/.gitignore_global
-ln -s "$(pwd)/inputrc" ~/.inputrc
-ln -s "$(pwd)/gpg.conf" ~/.gnupg/
-ln -s "$(pwd)/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -s "${DOTFILE_DIR}/bash_profile" ~/.bash_profile
+ln -s "${DOTFILE_DIR}/bashrc" ~/.bashrc
+ln -s "${DOTFILE_DIR}/git-prompt.sh" ~/.git-prompt.sh
+ln -s "${DOTFILE_DIR}/gitconfig" ~/.gitconfig
+ln -s "${DOTFILE_DIR}/gitignore_global" ~/.gitignore_global
+ln -s "${DOTFILE_DIR}/inputrc" ~/.inputrc
+ln -s "${DOTFILE_DIR}/gpg.conf" ~/.gnupg/
+ln -s "${DOTFILE_DIR}/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
