dda9e3c6e84176d6f8dbcb23f0217c24ff3eafdc
camercu <cameron.unterberger@gmail.com>
2020-05-29 16:23:31 -0400

    clean up old junk

--- deploy.sh
+++ deploy.sh
@@ -7,26 +7,14 @@ ARCHIVE_DIR="${DOTFILE_DIR}/old"
 [[ -d $ARCHIVE_DIR ]] || mkdir -p "$ARCHIVE_DIR"
 
 # back up existing dotfiles
-[[ -f ~/.bash_aliases && ! -L ~/.bash_aliases ]] && \
-    mv ~/.bash_aliases "$ARCHIVE_DIR"
-[[ -f ~/.bash_profile && ! -L ~/.bash_profile ]] && \
-	mv ~/.bash_profile "$ARCHIVE_DIR"
-[[ -f ~/.bashrc && ! -L ~/.bashrc ]] && \
-	mv ~/.bashrc "$ARCHIVE_DIR"
 [[ -f ~/.gdbinit && ! -L ~/.gdbinit ]] && \
 	mv ~/.gdbinit "$ARCHIVE_DIR"
-[[ -f ~/.git-prompt.sh && ! -L ~/.git-prompt.sh ]] && \
-	mv ~/.git-prompt.sh "$ARCHIVE_DIR"
 [[ -f ~/.gitconfig && ! -L ~/.gitconfig ]] && \
 	mv ~/.gitconfig "$ARCHIVE_DIR"
-[[ -f ~/.gitconfig.aliases && ! -L ~/.gitconfig.aliases ]] && \
+[[ -f ~/.gitconfig-aliases && ! -L ~/.gitconfig-aliases ]] && \
 	mv ~/.gitconfig.aliases "$ARCHIVE_DIR"
-[[ -f ~/.gitignore_global && ! -L ~/.gitignore_global ]] && \
+[[ -f ~/.gitignore-global && ! -L ~/.gitignore-global ]] && \
 	mv ~/.gitignore_global "$ARCHIVE_DIR"
-[[ -f ~/.inputrc && ! -L ~/.inputrc ]] && \
-	mv ~/.inputrc "$ARCHIVE_DIR"
-[[ -f ~/.nanorc && ! -L ~/.nanorc ]] && \
-	mv ~/.nanorc "$ARCHIVE_DIR"
 [[ -f ~/.tmux.conf && ! -L ~/.tmux.conf ]] && \
 	mv ~/.tmux.conf "$ARCHIVE_DIR"
 [[ -f ~/.vimrc && ! -L ~/.vimrc ]] && \
@@ -41,17 +29,11 @@ ARCHIVE_DIR="${DOTFILE_DIR}/old"
 	mv ~/.zsh-aliases "$ARCHIVE_DIR"
 
 # symlink dotfiles
-[[ -e ~/.bash_aliases ]] || ln -s "${DOTFILE_DIR}/bash_aliases" ~/.bash_aliases
-[[ -e ~/.bash_profile ]] || ln -s "${DOTFILE_DIR}/bash_profile" ~/.bash_profile
-[[ -e ~/.bashrc ]] || ln -s "${DOTFILE_DIR}/bashrc" ~/.bashrc
 [[ -e ~/.gdbinit ]] || ln -s "${DOTFILE_DIR}/gdbinit" ~/.gdbinit
-[[ -e ~/.git-prompt.sh ]] || ln -s "${DOTFILE_DIR}/git-prompt.sh" ~/.git-prompt.sh
 [[ -e ~/.gitconfig ]] || ln -s "${DOTFILE_DIR}/gitconfig" ~/.gitconfig
-[[ -e ~/.gitconfig.aliases ]] || ln -s "${DOTFILE_DIR}/gitconfig.aliases" ~/.gitconfig.aliases
-[[ -e ~/.gitignore_global ]] || ln -s "${DOTFILE_DIR}/gitignore_global" ~/.gitignore_global
+[[ -e ~/.gitconfig-aliases ]] || ln -s "${DOTFILE_DIR}/gitconfig-aliases" ~/.gitconfig-aliases
+[[ -e ~/.gitignore-global ]] || ln -s "${DOTFILE_DIR}/gitignore-global" ~/.gitignore-global
 [[ ! -e ~/.gnupg/gpg.conf && -e ~/.gnupg ]] && ln -s "${DOTFILE_DIR}/gpg.conf" ~/.gnupg/
-[[ -e ~/.inputrc ]] || ln -s "${DOTFILE_DIR}/inputrc" ~/.inputrc
-[[ -e ~/.nanorc ]] || ln -s "${DOTFILE_DIR}/nanorc" ~/.nanorc
 [[ -e ~/.tmux.conf ]] || ln -s "${DOTFILE_DIR}/tmux.conf" ~/.tmux.conf
 [[ -e ~/.vimrc ]] || ln -s "${DOTFILE_DIR}/vimrc" ~/.vimrc
 [[ -e ~/.vimrc-plugs ]] || ln -s "${DOTFILE_DIR}/vimrc-plugs" ~/.vimrc-plugs
