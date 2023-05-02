9492a59f238016f7d400cd29e805ba5619a119bd
camercu <cameron.unterberger@gmail.com>
2023-05-02 16:24:15 -0500

    fix typo

--- install.sh
+++ install.sh
@@ -141,7 +141,7 @@ LINUX_DOTFILES=(
 	.mozilla/firefox/profiles.ini
 	.mozilla/firefox/user.js
 	.mozilla/firefox/extensions/{60f82f00-9ad5-4de5-b31c-b16a47c51558}.xpi
-	.mozilla/firefox/extension/foxyproxy@eric.h.jung.xpi
+	.mozilla/firefox/extensions/foxyproxy@eric.h.jung.xpi
 )
 
 # install Linux-specific dotfiles
