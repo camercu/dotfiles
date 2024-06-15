f95a746c97ca1201377cf6d2d6bce95af1f9020b
camercu <cameron.unterberger@gmail.com>
2024-06-15 12:19:36 -0500

    add pyright lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -398,11 +398,14 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- pyright = {},
+        pyright = {},
         bashls = {},
         dockerls = {},
         gopls = {},
-        ruff = {},
+        ruff = {
+          -- cmd = { 'ruff', 'server', '--preview', '--config', "lint.extend-select = ['W', 'I']" },
+          -- settings = { fix = true, lint = { ['extend-select'] = { 'W', 'I' } } },
+        },
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
