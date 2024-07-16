fc09c9ad126ab1a6c66829d9642c9b4344c9cf7a
camercu <cameron.unterberger@gmail.com>
2024-07-15 22:04:37 -0500

    update which-key maps to new format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -92,21 +92,31 @@ require('lazy').setup({
 
       -- Document existing key chains
       require('which-key').register({
-        ['<leader><tab>'] = { name = '[Tab]', _ = 'which_key_ignore' },
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>o'] = { name = '[O]pen', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]efactor', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]urround', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        { '<leader><tab>', group = '[Tab]' },
+        { '<leader><tab>_', hidden = true },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>f', group = '[F]ind' },
+        { '<leader>f_', hidden = true },
+        { '<leader>h', group = 'Git [H]unk' },
+        { '<leader>h_', hidden = true },
+        { '<leader>o', group = '[O]pen' },
+        { '<leader>o_', hidden = true },
+        { '<leader>r', group = '[R]efactor' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]urround' },
+        { '<leader>s_', hidden = true },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>t_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
       })
       -- visual mode
       require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+      })
     end,
   },
 
