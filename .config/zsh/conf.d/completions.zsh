ef2c21a1d101e00159fddef33dd58b51036c2494
camercu <cameron.unterberger@gmail.com>
2024-09-01 23:38:41 -0500

    clean up autocomplete settings

--- .config/zsh/conf.d/completions.zsh
+++ .config/zsh/conf.d/completions.zsh
@@ -4,14 +4,16 @@
 # Tab Completions
 #
 
-# give bindkey access to 'menuselect', must be called before compinit
-zmodload zsh/complist
+# give bindkey access to 'menuselect'
+# must be called before compinit or setting menuselect keybindings
+zmodload -i zsh/complist
 
 
 #
 # Options
 #
 # https://zsh.sourceforge.io/Doc/Release/Options.html#Completion-4
+# man zshoptions - Search for "Completion"
 
 setopt ALWAYS_TO_END        # Move cursor to end of completion suggestion.
 setopt AUTO_LIST            # Automatically list choices on ambiguous completion
@@ -23,23 +25,27 @@ unsetopt MENU_COMPLETE      # Do not autoselect the first completion entry
 #
 # Matcher Options
 #
+# man zshmodules - Search for "zstyle"
+# man zshcompsys - Search for "Standard Styles"
+# man zshcompsys - Search for "Standard Tags"
 
 # completers
 zstyle ':completion:*' completer _extensions _complete _approximate
 
 # use select-style menu to pick completion
-zstyle ':completion:*:*:*:*:*' menu select search
+zstyle ':completion:*:*:*:*:*' menu select interactive
 
-# autocomplete options instead of directory stack
+# autocomplete options instead of directory stack when doing hyphen-TAB
 zstyle ':completion:*' complete-options true
 
 # sort files by modification datetime instead of alphabetically
-zstyle ':completion:*' file-sort modification
+zstyle ':completion:*' file-sort modification follow
 
 # prefer exact match first (`''` after matcher-list),
 # try case & hyphen/underscore insensitive,
 # allow partial completion before ._-,
 # allow partial completion from end of word
+# man zshcompwid - Search for "COMPLETION MATCHING CONTROL"
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # formatted labels on groups in completion menu
@@ -48,13 +54,6 @@ zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
 zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
 # zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-
-# Colors for files and directories
-zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
-# zstyle ':completion:*' list-colors ''
-
-# colors for `kill` processes
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
 # autocomplete processes using `ps` output
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USERNAME -o pid,user,comm -w -w"
@@ -67,6 +66,9 @@ zstyle ':completion:*' group-name ''
 
 # ordering of groups for command completion
 zstyle ':completion:*:*:-command-:*:*' group-order aliases functions builtins commands
+
+# don't treat multiple slashes as glob directory path
+zstyle ':completion:*' squeeze-slashes true
 
 # don't auto-expand tilde to home-directory
 zstyle ':completion:*' keep-prefix true
@@ -87,6 +89,35 @@ zstyle ':completion:*:*:*:users' ignored-patterns \
 
 # ... unless we really want to.
 zstyle '*' single-ignored show
+
+#
+# Colors
+#
+# man zshmodules - Search "Colored completion listings"
+
+# Colors for files and directories
+zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+# zstyle ':completion:*' list-colors ''
+
+# colors for `kill` processes
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+
+
+#
+# Complist Menu keybindings
+#
+# man zshmodules - Search for “THE ZSH/COMPLIST MODULE”
+
+bindkey -M menuselect '^M' accept-line                              # Enter to accept
+bindkey -M menuselect '^[^M' .accept-line                           # A-Enter to accept and execute
+bindkey -M menuselect '^xg' clear-screen                            # "C-x g" to clear screen
+bindkey -M menuselect '^xi' vi-insert                               # Insert
+bindkey -M menuselect '^xh' accept-and-hold                         # Hold
+bindkey -M menuselect '^xn' accept-and-infer-next-history           # Next
+bindkey -M menuselect '^xu' undo                                    # Undo
+bindkey -M menuselect '?' history-incremental-search-forward        # next search result
+bindkey -M menuselect '/' history-incremental-search-forward        # prev search result
 
 
 #
