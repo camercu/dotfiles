a1c2a233339613d68cb0d05d1094783110df60c2
camercu <cameron.unterberger@gmail.com>
2025-01-02 23:51:52 -0600

    add trackpad tap to click

--- scripts/macos-config.zsh
+++ scripts/macos-config.zsh
@@ -53,7 +53,10 @@ defaults write com.apple.dock "wvous-bl-modifier" -int 0
 
 # Trackpad Settings
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain "com.apple.mouse.tapBehavior" -int 1
 defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
 
