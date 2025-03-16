1aa0176d653449150005d079bb04c34604b48ca3
camercu <cameron.unterberger@gmail.com>
2025-03-15 22:08:52 -0500

    more bat config

--- common/.config/zsh/env/bat.zsh
+++ common/.config/zsh/env/bat.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
 if command -v bat &>/dev/null; then
-  BAT_THEME="Catppuccin Mocha"
+  BAT_THEME="Catppuccin Frappe"
+
+  alias bathelp='bat --plain --language=help'
+
+  function help {
+      "$@" --help 2>&1 | bathelp
+  }
+
+  alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+  alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 fi
