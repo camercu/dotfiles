0bec14cb21a14df94cb958328af9ef98272a0505
camercu <cameron.unterberger@gmail.com>
2025-03-16 00:26:47 -0500

    add emoji

---
+++ common/.config/nvim/lua/plugins/emoji.lua
@@ -0,0 +1,25 @@
+return {
+  'allaman/emoji.nvim',
+  lazy = true,
+  ft = { 'markdown', 'text', 'plaintex', 'gitcommit', 'scratch' },
+  dependencies = {
+    -- util for handling paths
+    'nvim-lua/plenary.nvim',
+    -- optional for telescope integration
+    'nvim-telescope/telescope.nvim',
+    -- optional for fzf-lua integration via vim.ui.select
+    'ibhagwan/fzf-lua',
+    -- optional for blink.cmp integration
+    'saghen/blink.cmp',
+  },
+  opts = {
+    -- default is false, also needed for blink.cmp integration!
+    enable_cmp_integration = true,
+  },
+  config = function(_, opts)
+    require('emoji').setup(opts)
+    -- optional for telescope integration
+    local ts = require('telescope').load_extension('emoji')
+    vim.keymap.set('n', '<leader>se', ts.emoji, { desc = '[S]earch [E]moji' })
+  end,
+}
--- common/.config/nvim/lua/plugins/extend-blink-cmp.lua
+++ common/.config/nvim/lua/plugins/extend-blink-cmp.lua
@@ -5,6 +5,24 @@ return {
       keymap = {
         preset = 'super-tab',
       },
+      sources = {
+        default = { 'emoji' },
+        providers = {
+          emoji = {
+            name = 'emoji',
+            module = 'blink.compat.source',
+            -- overwrite kind of suggestion
+            transform_items = function(ctx, items)
+              local kind = require('blink.cmp.types').CompletionItemKind.Text
+              for i = 1, #items do
+                items[i].kind = kind
+              end
+              return items
+            end,
+          },
+        },
+      },
     },
+    dependencies = { 'allaman/emoji.nvim', 'saghen/blink.compat' },
   },
 }
