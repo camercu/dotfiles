4ee570c1faf9663bbf3a35249125985b63917976
camercu <cameron.unterberger@gmail.com>
2026-01-01 10:33:11 -0600

    add bacon plugin to nvim

---
+++ common/.config/nvim/lua/plugins/bacon.lua
@@ -0,0 +1,14 @@
+return {
+  "Canop/nvim-bacon",
+  config = function()
+    require("bacon").setup({
+      quickfix = {
+        enabled = true, -- Enable Quickfix integration
+        event_trigger = true, -- Trigger QuickFixCmdPost after populating Quickfix list
+      },
+    })
+    local map = LazyVim.safe_keymap_set
+    map("n", "!", ":BaconLoad<CR>:w<CR>:BaconNext<CR>", { desc = "Navigate to next bacon location" })
+    map("n", "<leader>cb", ":BaconList<CR>", { desc = "Open bacon locations list" })
+  end,
+}
