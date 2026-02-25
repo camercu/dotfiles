0a04fa41e59ff890135a194496e27096368e656d
camercu <cameron.unterberger@gmail.com>
2026-02-24 22:20:40 -0600

    fix(zsh): harden and correct history settings
      Raise SAVEHIST from 10k to 50k to match HISTSIZE, preventing history
      loss when a session accumulates many commands. Add HIST_IGNORE_SPACE
      so commands prefixed with a space are never recorded.

--- common/.config/zsh/env/history.zsh
+++ common/.config/zsh/env/history.zsh
@@ -5,5 +5,7 @@
 #
 export HISTFILE="$__zsh_cache_dir/.zsh_history"    # History filepath
 export HISTSIZE=50000                   # Maximum events for internal history
-export SAVEHIST=10000                   # Maximum events in history file
+export SAVEHIST=50000                   # Maximum events in history file
+
+setopt HIST_IGNORE_SPACE                # Don't record commands prefixed with a space
 
