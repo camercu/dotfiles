c7363afa3e3219edf2b71329c0a7eef61002f562
camercu <cameron.unterberger@gmail.com>
2026-02-24 22:20:54 -0600

    perf(zsh): lazy-load NVM to eliminate startup cost
      Replace eager source of nvm.sh with stub functions for nvm, node,
      npm, npx, yarn, pnpm, and corepack. NVM is sourced only on first
      use, saving ~200-500ms per shell startup.

--- common/.config/zsh/env/nvm.zsh
+++ common/.config/zsh/env/nvm.zsh
@@ -2,4 +2,16 @@
 
 # enable nvm
 export NVM_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+
+# Lazy-load nvm: source on first use of any node-related command
+_nvm_load() {
+    unset -f nvm node npm npx yarn pnpm corepack
+    [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+}
+nvm()      { _nvm_load; nvm      "$@"; }
+node()     { _nvm_load; node     "$@"; }
+npm()      { _nvm_load; npm      "$@"; }
+npx()      { _nvm_load; npx      "$@"; }
+yarn()     { _nvm_load; yarn     "$@"; }
+pnpm()     { _nvm_load; pnpm     "$@"; }
+corepack() { _nvm_load; corepack "$@"; }
