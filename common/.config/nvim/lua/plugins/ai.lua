8188ccba19df35848daa9cf33b348008f1e140e6
camercu <cameron.unterberger@gmail.com>
2026-05-31 11:34:33 -0500

    clean up AI models to latest in use

--- common/.config/nvim/lua/plugins/ai.lua
+++ common/.config/nvim/lua/plugins/ai.lua
@@ -49,16 +49,6 @@ return {
           end,
         },
         acp = {
-          opencode = function()
-            return require("codecompanion.adapters.acp").extend("opencode", {
-              schema = {
-                model = {
-                  default = "anthropic/claude-opus-4-6/high",
-                },
-              },
-            })
-          end,
-
           claude_code = function()
             local command
 
@@ -80,45 +70,9 @@ return {
               commands = {
                 default = command,
               },
-              env = {
-                CLAUDE_CODE_OAUTH_TOKEN = os.getenv("CLAUDE_CODE_OAUTH_TOKEN"),
-              },
               schema = {
                 model = {
-                  default = "anthropic/claude-opus-4-6/high",
-                },
-              },
-            })
-          end,
-
-          codex = function()
-            local command
-
-            if vim.fn.executable("codex-acp") == 1 then
-              command = { vim.fn.exepath("codex-acp") }
-            elseif vim.fn.executable("npx") == 1 then
-              command = { "npx", "-y", "@zed-industries/codex-acp" }
-            else
-              vim.schedule(function()
-                vim.notify("CodeCompanion Codex adapter requires `codex-acp` or `npx`.", vim.log.levels.ERROR)
-              end)
-              command = { "codex-acp" }
-            end
-
-            return require("codecompanion.adapters.acp").extend("codex", {
-              commands = {
-                default = command,
-              },
-              defaults = {
-                auth_method = "chatgpt", -- "openai-api-key"|"codex-api-key"|"chatgpt"
-              },
-              env = {
-                OPENAI_API_KEY = os.getenv("OPENAI_API_KEY"),
-                CODEX_API_KEY = os.getenv("CODEX_API_KEY"),
-              },
-              schema = {
-                model = {
-                  default = "gpt-5.3-codex/high",
+                  default = "anthropic/claude-opus-4-8/high",
                 },
               },
             })
@@ -139,7 +93,7 @@ return {
         cmd = {
           adapter = {
             name = "ollama",
-            model = "qwen2.5-coder:7b-base-q6_K",
+            model = "codellama:7b-code",
           },
         },
       },
