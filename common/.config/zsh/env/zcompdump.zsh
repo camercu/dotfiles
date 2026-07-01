5b84f6fed83bf8e233f3773126451528449a9637
camercu <cameron.unterberger@gmail.com>
2026-06-30 20:13:27 -0400

    refactor(zsh): remove redundant env/zcompdump.zsh
    .zshrc already sets ZSH_COMPDUMP to the same path and creates its dir; the
    file also referenced a non-existent XDG_CACHE variable.

--- common/.config/zsh/env/zcompdump.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-typeset -g __zsh_compdump_cache_root="${XDG_CACHE_HOME:-${XDG_CACHE:-$HOME/.cache}}"
-typeset -g __zsh_compdump_dir="${__zsh_compdump_cache_root}/zsh"
-[[ -d "$__zsh_compdump_dir" ]] || mkdir -p -- "$__zsh_compdump_dir"
-export ZSH_COMPDUMP="${__zsh_compdump_dir}/.zcompdump-${HOST}"
