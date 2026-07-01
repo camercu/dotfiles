d219d97a3ae16b9f6b75f3ac85a6a8aff63f2bca
camercu <cameron.unterberger@gmail.com>
2026-06-30 20:04:46 -0400

    fix(zsh): stop exporting color name globals into environment
    typeset -gx leaked RED/WHITE/BLACK/etc to every child process, clashing
    with programs that read those names. Consumers run in-shell; -g suffices.

--- common/.config/zsh/env/colors.zsh
+++ common/.config/zsh/env/colors.zsh
@@ -3,7 +3,9 @@
 # enable coloring in terminal
 export CLICOLOR=1
 
-# Load Color constants into environment
+# Load Color constants as shell globals (NOT exported).
+# Exporting names like RED/WHITE/BLACK pollutes every child process and clashes
+# with programs that read those variables. Consumers here run in-shell.
 #
 # To read source of colors function, do:
 # less $^fpath/colors(N)
@@ -11,7 +13,7 @@ export CLICOLOR=1
 # adapted from: https://stackoverflow.com/a/6159885
 autoload -U colors && colors
 for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
-    typeset -gx "${COLOR}=${fg_no_bold[${(L)COLOR}]}"
-    typeset -gx "BOLD_${COLOR}=${fg_bold[${(L)COLOR}]}"
+    typeset -g "${COLOR}=${fg_no_bold[${(L)COLOR}]}"
+    typeset -g "BOLD_${COLOR}=${fg_bold[${(L)COLOR}]}"
 done
-typeset -gx RESET="$reset_color"
+typeset -g RESET="$reset_color"
