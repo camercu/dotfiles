82d106174df6433d32b6cb15621674486c107b59
camercu <camercu@users.noreply.github.com>
2026-09-16 06:14:15 -0400

    fix(nvim): drop telescope for emoji picker

--- common/.config/nvim/lua/plugins/emoji.lua
+++ common/.config/nvim/lua/plugins/emoji.lua
@@ -5,10 +5,6 @@ return {
   dependencies = {
     -- util for handling paths
     "nvim-lua/plenary.nvim",
-    -- optional for telescope integration
-    "nvim-telescope/telescope.nvim",
-    -- optional for fzf-lua integration via vim.ui.select
-    -- 'ibhagwan/fzf-lua',
     -- optional for blink.cmp integration
     "saghen/blink.cmp",
   },
@@ -18,8 +14,17 @@ return {
   },
   config = function(_, opts)
     require("emoji").setup(opts)
-    -- optional for telescope integration
-    local ts = require("telescope").load_extension("emoji")
-    vim.keymap.set("n", "<leader>se", ts.emoji, { desc = "[S]earch [E]moji" })
+    -- vim.ui.select is backed by the snacks picker (fuzzy search included),
+    -- so no telescope dependency needed just for this one command.
+    vim.keymap.set("n", "<leader>se", function()
+      vim.ui.select(require("emoji.data").emoji_items(), {
+        prompt = "Emoji",
+        format_item = function(item)
+          return item.label
+        end,
+      }, function(item)
+        if item then require("emoji.utils").insert_string_at_current_cursor(item.character) end
+      end)
+    end, { desc = "[S]earch [E]moji" })
   end,
 }
